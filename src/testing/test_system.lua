1e443584a35c26a12705736e3fe5c51820df5f36
Michael Allwright <allsey87@gmail.com>
2021-11-22 17:26:11 +0100

    Seed the RNG with the current time if no seed is provided

--- src/testing/test_system.lua
+++ src/testing/test_system.lua
@@ -1,25 +1,14 @@
---[[ This function is executed every time you press the 'execute' button ]]
 function init()
 end
 
-
---[[ This function is executed at each time step
-     It must contain the logic of your controller ]]
 function step()
    log("time = " .. string.format("%.1f", robot.system.time) .. " seconds");
    log("temperature = " .. string.format("%.1f", robot.system.temperature) .. " degrees");
+   log("random integer = " .. string.format("%.0f", robot.random.uniform_int(0,20)));
 end
 
---[[ This function is executed every time you press the 'reset'
-     button in the GUI. It is supposed to restore the state
-     of the controller to whatever it was right after init() was
-     called. The state of sensors and actuators is reset
-     automatically by ARGoS. ]]
 function reset()
 end
 
---[[ This function is executed only once, when the robot is removed
-     from the simulation ]]
 function destroy()
-   -- put your code here
 end
